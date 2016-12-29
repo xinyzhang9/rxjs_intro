894233547522a7347bf2849c576cfaccd7de383b
Xinyang <xinyzhang9@gmail.com>
2016-12-28 23:48:12 -0800

    generator

--- createObservables/main.js
+++ createObservables/main.js
@@ -6,21 +6,33 @@
 
 // console.log(foo.call());
 
-var bar = Rx.Observable.create(function(observer){
+// var bar = Rx.Observable.create(function(observer){
+// 	console.log('hello');
+// 	observer.next(42);
+// 	observer.next(100);
+// 	observer.next(200);
+
+// 	setTimeout(function(){
+// 		observer.next(300);
+// 	},1000)
+// });
+
+// console.log('before');
+
+// bar.subscribe(function(x){
+// 	console.log(x);
+// });
+
+// console.log('after');
+
+// generator
+function* baz(){
 	console.log('hello');
-	observer.next(42);
-	observer.next(100);
-	observer.next(200);
+	yield 42;
+	yield 100;
+	yield 200;
+}
 
-	setTimeout(function(){
-		observer.next(300);
-	},1000)
-});
-
-console.log('before');
-
-bar.subscribe(function(x){
-	console.log(x);
-});
-
-console.log('after');
+var iterator = baz();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
