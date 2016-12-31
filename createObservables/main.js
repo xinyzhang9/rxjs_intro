66d0ecf1df6499f759748fd145cf3860a3e59605
Xinyang <xinyzhang9@gmail.com>
2016-12-31 01:36:53 -0800

    err

--- createObservables/main.js
+++ createObservables/main.js
@@ -7,34 +7,43 @@
 // console.log(foo.call());
 
 // producer determine when the values are sent
-// var bar = Rx.Observable.create(function(observer){
-// 	console.log('hello');
-// 	observer.next(42);
-// 	observer.next(100);
-// 	observer.next(200);
+var bar = Rx.Observable.create(function(observer){
+	try{
+		console.log('hello');
+		observer.next(42);
+		observer.next(100);
+		observer.next(200);
 
-// 	setTimeout(function(){
-// 		observer.next(300);
-// 	},1000)
-// });
+		
+
+		setTimeout(function(){
+			observer.next(300);
+		},1000)
+	}catch(err){
+		observer.error(err);
+	}
+	
+});
 
 // console.log('before');
 
-// bar.subscribe(function(x){
-// 	console.log(x);
-// });
+bar.subscribe(function nextValueHandler(x){
+	console.log(x);
+},function errorHandler(err){
+	console.log('Something went wrong: '+ err);
+});
 
 // console.log('after');
 
 // generator
-function* baz(){
-	console.log('hello');
-	yield 42;
-	yield 100;
-	yield 200;
-}
+// function* baz(){
+// 	console.log('hello');
+// 	yield 42;
+// 	yield 100;
+// 	yield 200;
+// }
 
 // consumer determine when the values are sent
-var iterator = baz();
-console.log(iterator.next().value);
-console.log(iterator.next().value);
+// var iterator = baz();
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
