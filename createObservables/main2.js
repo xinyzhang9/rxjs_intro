7168a31270fb5872e6dc1bbbf9cd34cbb992aad2
Xinyang <xinyzhang9@gmail.com>
2016-12-31 01:54:05 -0800

    from event

--- createObservables/main2.js
+++ createObservables/main2.js
@@ -1,5 +1,22 @@
 // of
-var foo = Rx.Observable.of(42,100,200);
+// var foo = Rx.Observable.of(42,100,200);
+
+
+//from event
+function addEventHandler(handler){
+	document.addEventListener('click',handler);
+}
+
+function removeEventHandler(handler){
+	document.removeEventListener('click',handler);
+}
+
+// var foo = Rx.Observable.fromEventPattern(
+// 	addEventHandler, removeEventHandler
+// )
+
+
+var foo = Rx.Observable.fromEvent(document,'click');
 
 foo.subscribe(function(x){
 	console.log('next '+x);
