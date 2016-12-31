9681ce3c516a630e3d9156ae03b1fd51552a14a4
Xinyang <xinyzhang9@gmail.com>
2016-12-31 01:38:07 -0800

    handle complete

--- createObservables/main.js
+++ createObservables/main.js
@@ -13,8 +13,7 @@ var bar = Rx.Observable.create(function(observer){
 		observer.next(42);
 		observer.next(100);
 		observer.next(200);
-
-		
+		observer.complete();		
 
 		setTimeout(function(){
 			observer.next(300);
@@ -31,6 +30,8 @@ bar.subscribe(function nextValueHandler(x){
 	console.log(x);
 },function errorHandler(err){
 	console.log('Something went wrong: '+ err);
+},function completeHandler(){
+	console.log('done');
 });
 
 // console.log('after');
