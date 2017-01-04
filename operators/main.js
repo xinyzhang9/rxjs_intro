d7b73fcf43b5fea942b73a33c5df2345e007a97f
Xinyang <xinyzhang9@gmail.com>
2017-01-04 14:58:56 -0800

    scan

--- operators/main.js
+++ operators/main.js
@@ -6,10 +6,12 @@ var bar = Rx.Observable.interval(400).take(5);
 ---0---1---2---3---4|
 	zip((x,y)=>x)
 ---h---e---l---l---o|
+	scan((acc,x)=>axx+x,'')
+---h---(he)---(hel)---(hell)---(hello)
 
 */
 
-var combined = foo.zip(bar,(x,y)=>x);
+var combined = foo.zip(bar,(x,y)=>x).scan((acc,x)=>acc+x,'');
 
 combined.subscribe(
 	function(x){ console.log('next '+x); },
