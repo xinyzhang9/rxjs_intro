df35c61ac61a36a6ee8e7efe9c538b9c3805c1b1
Xinyang <xinyzhang9@gmail.com>
2017-01-03 16:21:43 -0800

    combineLatest

--- operators/main.js
+++ operators/main.js
@@ -1,14 +1,17 @@
-var foo = Rx.Observable.interval(500).take(4);
-var bar = Rx.Observable.interval(300).take(5);
+var weight = Rx.Observable.interval(500).take(5);
+var height = Rx.Observable.interval(500).take(5);
 
 /*
-
+----0----1----2----(3|)
+--0--1--2--3--(4|)
+	combineLatest((x,y)=> x+y) (AND style)
+----01--23-4--(56)-(7|)	
 */
 
 
-var merged = Rx.Observable.merge(foo,bar);
+var bmi = weight.combineLatest(height,(w,h) => w * h);
 
-merged.subscribe(
+bmi.subscribe(
 	function(x){ console.log('next '+x); },
 	function(err){ console.log('error '+err); },
 	function(x){ console.log('done'); }
