e369f080264b2d4f44a4d920675baa52881364be
Xinyang <xinyzhang9@gmail.com>
2017-01-04 15:25:09 -0800

    buffer

--- operators/main.js
+++ operators/main.js
@@ -1,17 +1,19 @@
-var foo = Rx.Observable.of('l','o','v','e','u');
-var bar = Rx.Observable.interval(400).take(5);
+var foo = Rx.Observable.of('l','o','v','e','u')
+	.zip(Rx.Observable.interval(600).take(5),(x,y)=>x);
 
+var bar = Rx.Observable.interval(900).take(3);
 /*
 (hello|)
 ---0---1---2---3---4|
 	zip((x,y)=>x)
 ---h---e---l---l---o|
-	scan((acc,x)=>axx+x,'')
----h---(he)---(hel)---(hell)---(hello)
+	bufferCount(2)
+-------he------ll---([o]|)
 
 */
 
-var combined = foo.zip(bar,(x,y)=>x).scan((acc,x)=>acc+x,'');
+// var combined = foo.zip(bar,(x,y)=>x).scan((acc,x)=>acc+x,'');
+var combined = foo.buffer(bar);
 
 combined.subscribe(
 	function(x){ console.log('next '+x); },
