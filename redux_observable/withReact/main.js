0ea815c64aa879c79beb5b0cedaf04ca823d4224
Xinyang <xinyzhang9@gmail.com>
2017-01-23 00:49:51 -0800

    withReact

---
+++ redux_observable/withReact/main.js
@@ -0,0 +1,47 @@
+const PING = 'PING';
+const PONG = 'PONG';
+
+const ping =  () => ({type: PING});
+
+const pingEpic = action$ =>
+	action$.ofType(PING)
+		.delay(1000)
+		.mapTo({type: PONG});
+
+const pingReducer = (state = { isPinging: false }, action) => {
+	switch(action.type){
+		case PING:
+			return { isPinging: true };
+		case PONG:
+			return { isPinging: false };
+		default:
+			return state;
+	}	
+};
+
+const { connect } = ReactRedux;
+let App = ({ isPinging, ping }) => (
+	<div>
+		<h1>is pinging: { isPinging.toString() }</h1>
+		<button onClick = { ping }>Start PING</button>
+	</div>
+);
+
+App = connect(
+	({ isPinging }) => ({ isPinging }),{ ping }
+)(App);
+
+const { Provider } = ReactRedux;
+const { createStore, applyMiddleware } = Redux;
+const { createEpicMiddleware } = ReduxObservable;
+
+const epicMiddleware = createEpicMiddleware(pingEpic);
+
+const store = createStore(pingReducer, applyMiddleware(epicMiddleware));
+
+ReactDOM.render(
+	<Provider store = {store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
