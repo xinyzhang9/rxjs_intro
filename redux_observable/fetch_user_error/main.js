36c402513f839e575a6d59bd253b0951c6f4dc7b
Xinyang <xinyzhang9@gmail.com>
2017-01-23 00:23:41 -0800

    handling error

--- redux_observable/fetch_user_error/main.js
+++ redux_observable/fetch_user_error/main.js
@@ -33,4 +33,81 @@ const fakeAjax = url => {
 	}
 };
 
-const fetchUserEpic = 
+const fetchUserEpic = action$ => 
+	action$.ofType(FETCH_USER)
+		.mergeMap(action => 
+			fakeAjax(`/api/users/${action.payload}`)
+				.map(fetchUserFulfilled)
+				.catch(error => Observable.of(
+					fetchUserRejected(error.xhr.response)
+				))
+		);
+
+// reducers
+const users = (state = {}, action) => {
+	switch(action.type){
+		case FETCH_USER:
+			return {};
+		case FETCH_USER_FULFILLED:
+			return {
+				...state,
+				[action.payload.id]: action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+const fetchUserError = (state = null, action) => {
+	switch(action.type){
+		case FETCH_USER:
+		case FETCH_USER_FULFILLED:
+			return null;
+		case FETCH_USER_REJECTED:
+			return action.payload;
+		default:
+			return state;
+	}
+};
+
+const { combineReducers, createStore, applyMiddleware } = Redux;
+const { combineEpics, createEpicMiddleware } = ReduxObservable;
+
+const rootReducer = combineReducers({users, fetchUserError});
+const rootEpic = combineEpics(fetchUserEpic);
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+const store = createStore(rootReducer, applyMiddleware(epicMiddleware));
+
+const renderApp = ()=> {
+	const userId = 'xz';
+	const { users, fetchUserError } = store.getState();
+	const user = users[userId] || '';
+
+	document.body.innerHTML = `
+		<div>
+			<h1>Fetch User Demo</h1>
+			<button onclick = "(${()=>{
+				store.dispatch(fetchUser('xz'));
+			}})();"
+			>
+				Fetch User Info
+			</button>
+
+			<div>
+				${fetchUserError ? '<p color = "red"> ERROR: '+ fetchUserError.message + '</p>' : ''}
+				${user && `<textarea>${JSON.stringify(user, null, 4)}</textarea>` }
+			</div>
+		</div>
+	`;
+};
+
+store.subscribe(renderApp);
+renderApp();
+
+
+
+
+
+
+
