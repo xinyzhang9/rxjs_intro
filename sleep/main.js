36c14c5520d1a109df5aee4ada71355bf19a4862
Xinyang <xinyzhang9@gmail.com>
2017-01-23 03:51:05 -0800

    make a card

--- sleep/main.js
+++ sleep/main.js
@@ -1,15 +1,11 @@
 let mousemove = Rx.Observable.fromEvent(document,'mousemove');
 let source = Rx.Observable.interval(1000).delay(2000).take(30).takeUntil(mousemove).repeat();
-
-let sleepSub = source.subscribe(
+const sleepSub = source.subscribe(
 	(x)=>{	
 			document.getElementById('res').style.visibility = 'visible';
-			document.getElementById('res').innerHTML = `You will be logged of after ${30-x} seconds`;
+			document.getElementById('text').innerHTML = `You will be logged off after ${30-x} seconds`;
 		}
 );
-
-let mouseSub = mousemove.subscribe(
-	(x) => {
-		document.getElementById('res').style.visibility = 'hidden';
-	}
-)
+const mouseSub = mousemove.subscribe(
+	(x) => { document.getElementById('res').style.visibility = 'hidden';}
+);
